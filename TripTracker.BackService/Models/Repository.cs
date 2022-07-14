ca69a60b690125288d557e215537ee4c4a6c0338
Bringfireh <ngutortsenongo@gmail.com>
2022-07-14 11:31:40 +0100

    added a repository class

--- TripTracker.BackService/Models/Repository.cs
+++ TripTracker.BackService/Models/Repository.cs
@@ -7,5 +7,35 @@ namespace TripTracker.BackService.Models
 {
     public class Repository
     {
+        private List<Trip> trips = new List<Trip>()
+        {
+            new Trip{
+            Id=1,
+            Name = "MVP Summit",
+            StartDate = new DateTime(2018,3,5),
+            EndDate = new DateTime(2018,3,8)
+            },
+            new Trip{
+                Id = 2,
+                Name = "DevIntersection Orlando 2018",
+                StartDate = new DateTime(2018,3,25),
+                EndDate =new DateTime(2018,3,28) 
+            },
+            new Trip
+            {
+                Id = 3,
+                Name="Build 2018",
+                StartDate = new DateTime(2018,5,7),
+                EndDate = new DateTime(2018,5,9)
+            }
+        };
+        public List<Trip> Get()
+        {
+            return trips;
+        }
+        public void Add(Trip mtrip)
+        {
+            
+        }
     }
 }
